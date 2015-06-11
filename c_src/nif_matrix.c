9a1ba9d2eb736860da5a683a6bf69b2646944838
Ross Jones <ross@servercode.co.uk>
2015-06-11 19:48:06 +0100

    No longer need UNUSED_FUNCTION

--- c_src/nif_matrix.c
+++ c_src/nif_matrix.c
@@ -4,12 +4,6 @@
 #  define UNUSED(x) UNUSED_ ## x
 #endif
 
-#ifdef __GNUC__
-#  define UNUSED_FUNCTION(x) __attribute__((__unused__)) UNUSED_ ## x
-#else
-#  define UNUSED_FUNCTION(x) UNUSED_ ## x
-#endif
-
 #include "erl_nif.h"
 
 #include <stdio.h>
